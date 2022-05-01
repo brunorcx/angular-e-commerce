28ed4c850ceaac7b94330a5bdde1bc6edbbf6f2c
brunorcx <brunorcx@hotmail.com>
2022-05-01 13:21:59 -0300

    feat: initial main product list layout (#4)
    * feat: material icons support and footer in a component
    
    * feat: initial footer layout
    
    * feat: finalized footer layout for PC
    
    * feat: initial product list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
 
+interface Products {
+  name: string;
+  price: number;
+  description: string;
+  rating: number;
+  image: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+  styleUrls: ['./app.component.less'],
 })
 export class AppComponent {
   title = 'angular-e-commerce';
+  products: Products[] = [
+    {
+      name: 'Product 1',
+      price: 50.0,
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.',
+      rating: 4.5,
+      image: 'assets/images/products/arroz.png',
+    },
+    {
+      name: 'Product 2',
+      price: 60.0,
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.',
+      rating: 3,
+      image: 'assets/images/products/arroz.png',
+    },
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/components/header/header.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent, FooterComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, MatIconModule],
   providers: [],
   bootstrap: [AppComponent],
 })
