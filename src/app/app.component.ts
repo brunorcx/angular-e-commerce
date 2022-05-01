fa856bf0d827982ccf46ee9bdf13a701b47682fb
brunorcx <brunorcx@hotmail.com>
2022-05-01 14:35:56 -0400

    feat: directives to control star icons based on rating

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -33,4 +33,14 @@ export class AppComponent {
       image: 'assets/images/products/arroz.png',
     },
   ];
+  counter(rating: number) {
+    let arrayRating: number[] = [];
+    for (let i = 0; i < rating; i++) {
+      //check if rating is a float
+      arrayRating.push(i + 1);
+    }
+    if (rating % 1 !== 0) arrayRating.push(6);
+
+    return arrayRating;
+  }
 }
