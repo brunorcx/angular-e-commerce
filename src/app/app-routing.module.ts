66f35a8070cd0aee4d8ab76f5e1f703239d615b2
Bruno RC <brunorcx@hotmail.com>
2022-05-04 20:15:25 -0400

    feat: initial app routing working for components suggested and manage-products

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { AppComponent } from './app.component';
+import { ManageProductsComponent } from './features/manage-products/manage-products.component';
+import { SuggestedComponent } from './features/suggested/suggested.component';
+const routes: Routes = [
+  { path: '', redirectTo: '/first-component', pathMatch: 'full' },
+  { path: 'first-component', component: SuggestedComponent },
+  { path: 'manage-products', component: ManageProductsComponent },
+  // { path: '**', component: AppComponent }, // redirect to home page on route error
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
