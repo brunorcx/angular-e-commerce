c1ecef019729b79a3b38ab3cf845527b818dc62b
Gabriel Machado <gramchado12@hotmail.com>
2022-05-15 12:54:42 -0400

    Created Sidebar

--- src/app/features/features.module.ts
+++ src/app/features/features.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { ManageProductsComponent } from './manage-products/manage-products.component';
 import { SuggestedComponent } from './suggested/suggested.component';
 import { MatIconModule } from '@angular/material/icon';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
-  declarations: [ManageProductsComponent, SuggestedComponent],
+  declarations: [ManageProductsComponent, SuggestedComponent, SidebarComponent],
   imports: [CommonModule, MatIconModule],
 })
 export class FeaturesModule {}
