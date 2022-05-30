ef947e1e55367af62beb4d7113bcfabc656e6903
brunorcx <brunorcx@hotmail.com>
2022-05-29 21:56:18 -0400

    Tags Injectable test working and passing data

--- src/app/core/services/tags.service.ts
+++ src/app/core/services/tags.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
 
+export interface Tags extends Array<string> {
+  [index: number]: string;
+}
+
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TagsService {
+  constructor() {
+    this.tags = [];
+  }
 
-  constructor() { }
+  tags: Tags;
+
+  getTags() {
+    return this.tags;
+  }
+  setTags(tags: Tags) {
+    console.log(tags);
+    this.tags = tags;
+  }
+  addTag(tag: string) {
+    this.tags.push(tag);
+  }
 }
